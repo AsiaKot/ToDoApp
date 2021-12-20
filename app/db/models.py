39f07f3eaabe650084960351c457b3d9cbf768a2
AsiaKot <joanna.kotwasinska@gmail.com>
2021-12-20 16:43:43 +0000

    init commit

--- app/db/models.py
+++ app/db/models.py
@@ -5,3 +5,9 @@ class ToDo(DB.Model):
     id_task = DB.Column(DB.Integer, primary_key=True)
     title = DB.Column(DB.String(100))
     complete = DB.Column(DB.Boolean)
+
+
+class NextWeek(DB.Model):
+    id_task = DB.Column(DB.Integer, primary_key=True)
+    title = DB.Column(DB.String(100))
+    complete = DB.Column(DB.Boolean)
--- app/server/routes.py
+++ app/server/routes.py
@@ -1,13 +1,18 @@
 from flask import render_template, request, redirect, url_for
 
-from app.db.models import ToDo, DB
+from app.db.models import ToDo, DB, NextWeek
 from . import SERVER_BLUEPRINT
+
+
+test = NextWeek(title="test", complete=False)
+DB.session.add(test)
 
 
 @SERVER_BLUEPRINT.route("/")
 def index():
     todo_list = ToDo.query.all()
-    return render_template("index.html", todo_list=todo_list)
+    next_week = NextWeek.query.all()
+    return render_template("index.html", todo_list=todo_list, next_week=next_week)
 
 
 @SERVER_BLUEPRINT.route("/add", methods=["POST"])
@@ -31,5 +36,15 @@ def update(todo_id_task):
 def delete(todo_id_task):
     todo = ToDo.query.filter_by(id_task=todo_id_task).first()
     DB.session.delete(todo)
+    DB.session.commit()
+    return redirect(url_for(".index"))
+
+
+@SERVER_BLUEPRINT.route("/move/<int:todo_id_task>")
+def move(todo_id_task):
+    todo = ToDo.query.filter_by(id_task=todo_id_task).first()
+    DB.session.delete(todo)
+    new_todo = NextWeek(title=todo.title, complete=todo.complete)
+    DB.session.add(new_todo)
     DB.session.commit()
     return redirect(url_for(".index"))
