3b431e076873f2ff072581d5011b62ee305e4a2e
AsiaKot <joanna.kotwasinska@gmail.com>
2021-12-20 18:30:34 +0000

    Created new tb ThisWeek & NextWeek

--- app/db/models.py
+++ app/db/models.py
@@ -7,6 +7,12 @@ class ToDo(DB.Model):
     complete = DB.Column(DB.Boolean)
 
 
+class ThisWeek(DB.Model):
+    id_task = DB.Column(DB.Integer, primary_key=True)
+    title = DB.Column(DB.String(100))
+    complete = DB.Column(DB.Boolean)
+
+
 class NextWeek(DB.Model):
     id_task = DB.Column(DB.Integer, primary_key=True)
     title = DB.Column(DB.String(100))
--- app/server/routes.py
+++ app/server/routes.py
@@ -1,14 +1,15 @@
 from flask import render_template, request, redirect, url_for
 
-from app.db.models import ToDo, NextWeek, DB
+from app.db.models import ToDo, ThisWeek, NextWeek, DB
 from . import SERVER_BLUEPRINT
 
 
 @SERVER_BLUEPRINT.route("/")
 def index():
     todo_list = ToDo.query.all()
+    this_week = ThisWeek.query.all()
     next_week = NextWeek.query.all()
-    return render_template("index.html", todo_list=todo_list, next_week=next_week)
+    return render_template("index.html", todo_list=todo_list, this_week=this_week, next_week=next_week)
 
 
 @SERVER_BLUEPRINT.route("/add", methods=["POST"])
@@ -40,7 +41,27 @@ def delete(todo_id_task):
 def move(todo_id_task):
     todo = ToDo.query.filter_by(id_task=todo_id_task).first()
     DB.session.delete(todo)
+    new_todo = ThisWeek(title=todo.title, complete=todo.complete)
+    DB.session.add(new_todo)
+    DB.session.commit()
+    return redirect(url_for(".index"))
+
+
+@SERVER_BLUEPRINT.route("/next_week/<int:todo_id_task>")
+def move_to_next_week(todo_id_task):
+    todo = ThisWeek.query.filter_by(id_task=todo_id_task).first()
+    DB.session.delete(todo)
     new_todo = NextWeek(title=todo.title, complete=todo.complete)
+    DB.session.add(new_todo)
+    DB.session.commit()
+    return redirect(url_for(".index"))
+
+
+@SERVER_BLUEPRINT.route("/previous_week/<int:todo_id_task>")
+def move_to_previous_week(todo_id_task):
+    todo = NextWeek.query.filter_by(id_task=todo_id_task).first()
+    DB.session.delete(todo)
+    new_todo = ThisWeek(title=todo.title, complete=todo.complete)
     DB.session.add(new_todo)
     DB.session.commit()
     return redirect(url_for(".index"))
