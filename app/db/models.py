c98c0bb2830b689e23995a44dc484c09aba4f286
AsiaKot <joanna.kotwasinska@gmail.com>
2021-12-21 15:05:58 +0000

    put all data in one tb (delete tb: ThisWeek, NextWeek)
    instead of keeping data in 3 tables, all data is kept in 1 - but with a new label "when" (Today, ThisWeek, NextWeek)

--- app/db/models.py
+++ app/db/models.py
@@ -5,15 +5,4 @@ class ToDo(DB.Model):
     id_task = DB.Column(DB.Integer, primary_key=True)
     title = DB.Column(DB.String(100))
     complete = DB.Column(DB.Boolean)
-
-
-class ThisWeek(DB.Model):
-    id_task = DB.Column(DB.Integer, primary_key=True)
-    title = DB.Column(DB.String(100))
-    complete = DB.Column(DB.Boolean)
-
-
-class NextWeek(DB.Model):
-    id_task = DB.Column(DB.Integer, primary_key=True)
-    title = DB.Column(DB.String(100))
-    complete = DB.Column(DB.Boolean)
+    when = DB.Column(DB.String(20))
--- app/server/routes.py
+++ app/server/routes.py
@@ -1,51 +1,45 @@
 from flask import render_template, request, redirect, url_for
 
-from app.db.models import ToDo, ThisWeek, NextWeek, DB
-from app.services.task_manger import filter_task, filter_tb
+from app.db.models import ToDo, DB
 from . import SERVER_BLUEPRINT
 
 
 @SERVER_BLUEPRINT.route("/")
 def index():
     todo_list = ToDo.query.all()
-    this_week = ThisWeek.query.all()
-    next_week = NextWeek.query.all()
     return render_template(
-        "index.html", todo_list=todo_list, this_week=this_week, next_week=next_week
-    )
+        "index.html", todo_list=todo_list)
 
 
 @SERVER_BLUEPRINT.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
-    new_todo = ToDo(title=title, complete=False)
+    new_todo = ToDo(title=title, complete=False, when="Today")
     DB.session.add(new_todo)
     DB.session.commit()
     return redirect(url_for(".index"))
 
 
-@SERVER_BLUEPRINT.route("/update/<class_name>/<int:todo_id_task>")
-def update(class_name, todo_id_task):
-    todo = filter_task(tb_key=class_name, todo_id_task=todo_id_task)
+@SERVER_BLUEPRINT.route("/update/<int:todo_id_task>")
+def update(todo_id_task):
+    todo = ToDo.query.filter_by(id_task=todo_id_task).first()
     todo.complete = not todo.complete
     DB.session.commit()
     return redirect(url_for(".index"))
 
 
-@SERVER_BLUEPRINT.route("/delete/<class_name>/<int:todo_id_task>")
-def delete(class_name, todo_id_task):
-    todo = filter_task(tb_key=class_name, todo_id_task=todo_id_task)
+@SERVER_BLUEPRINT.route("/delete/<int:todo_id_task>")
+def delete(todo_id_task):
+    todo = ToDo.query.filter_by(id_task=todo_id_task).first()
     DB.session.delete(todo)
     DB.session.commit()
     return redirect(url_for(".index"))
 
 
-@SERVER_BLUEPRINT.route("/<move_from>/<move_to>/<int:todo_id_task>")
-def move(move_from, move_to, todo_id_task):
-    todo = filter_task(tb_key=move_from, todo_id_task=todo_id_task)
-    DB.session.delete(todo)
-    new_todo = filter_tb(tb_key=move_to)(title=todo.title, complete=todo.complete)
-    DB.session.add(new_todo)
+@SERVER_BLUEPRINT.route("/move/<when>/<int:todo_id_task>")
+def move(when, todo_id_task):
+    todo = ToDo.query.filter_by(id_task=todo_id_task).first()
+    todo.when = when
     DB.session.commit()
     return redirect(url_for(".index"))
 
